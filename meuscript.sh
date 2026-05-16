b02854171a070e04fb5b448859015462f376de4a
Jacó Sampaio <72329574+JacoSampaio@users.noreply.github.com>
2026-05-15 23:47:30 -0300

    Update meuscript.sh

--- meuscript.sh
+++ meuscript.sh
@@ -3,3 +3,23 @@
 # Exemplo de um script bash
 echo "Arquivos no diretorio HOME"
 ls $HOME
+testando criptogravifia rsa
+Tutorial do Emacs. Veja no fim as condições para cópia.
+
+Os comandos do Emacs geralmente envolvem a tecla CONTROL (algumas vezes
+nomeada CTRL ou CTL) ou a tecla META (algumas vezes nomeada EDIT ou
+ALT). Antes de escrever isso tudo toda vez, usaremos as seguintes
+abreviações:
+
+ C-<chr>  Significa pressionar a tecla CONTROL enquanto digitar o
+          caractere <chr>.
+          Assim, C-f seria: Pressionar a tecla CONTROL e digitar f.
+ M-<chr>  Significa pressionar a tecla META, EDIT ou ALT enquanto
+          digitar <chr>.
+          Se não tiver META, EDIT ou ALT, no lugar digite ESC e depois
+          <chr>. Escrevemos <ESC> para tecla ESC.
+
+
+Nota importante: para terminar a sessão do Emacs, digite C-x C-c. (Dois
+caracteres.) Os caracteres ">>" na margem esquerda indicam direções para
+você tentar usar um comando.
